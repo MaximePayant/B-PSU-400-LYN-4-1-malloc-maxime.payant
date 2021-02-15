98d1841ef742524f7a7e6ba9fc1b1bd18e4e1052
Maxime Payant <maxime@pop-os.localdomain>
2021-02-15 13:29:32 +0100

    Add calloc.cpp

---
+++ calloc.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** Tek2
+** File description:
+** calloc.c
+*/
+
+#include "my_alloc.h"
+
+#include <unistd.h>
+
+void *calloc(size_t nmemb, size_t size)
+{
+    if (!nmemb || !size)
+        return NULL;
+    void *ptr = malloc(size * nmemb);
+    for (size_t ctr = 0; ctr < size * nmemb; ctr += 1)
+        ((char *)(ptr))[ctr] = 0;
+    return (ptr);
+}
